a1480890f1fa5809df98d6284a8120e5554b8387
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-27 11:00:13 +0530

    Validating for person data

--- src/main/java/com/example/addressbookapp/dto/ContactDTO.java
+++ src/main/java/com/example/addressbookapp/dto/ContactDTO.java
@@ -2,13 +2,24 @@ package com.example.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public @Data class ContactDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Address book is Invalid!!! ")
+    @NotEmpty(message = "first name cannot be null")
     public String firstName;
+    @Pattern(regexp = "^[A-Z]{1,}[a-zA-z\\s]{2,}$", message = "Address book is Invalid!!! ")
+    @NotEmpty(message = "last name cannot be null")
     public String lastName;
+    @Pattern(regexp = "^[A-Za-z,.0-9]{3,}$", message = "Address is Invalid!!! ")
+    @NotEmpty(message = "Address cannot be null")
     public String address;
     public String state;
     public String city;
+    @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$")
     public String zip;
+    @Pattern(regexp = "^^[0-9]{2}?[\\s,-]{0,1}[7-9]{1}[0-9]{9}$")
     public String phone;
 
     public ContactDTO(String firstName, String lastName, String address, String state, String city, String zip,
@@ -23,66 +34,67 @@ public @Data class ContactDTO {
         this.phone = phone;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
-                + state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
-    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public String getZip() {
+//        return zip;
+//    }
+//
+//    public void setZip(String zip) {
+//        this.zip = zip;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
+//                + state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
+//    }
 
 }
