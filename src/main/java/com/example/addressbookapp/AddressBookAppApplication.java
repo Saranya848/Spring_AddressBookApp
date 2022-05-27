4f670e221e4533e4b744dabbaaf7a4ff0edff10b
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-27 10:08:23 +0530

    Updated Evironment Variables

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,22 @@
 package com.example.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book App");
-        SpringApplication.run(AddressBookAppApplication.class, args);
+
+    public static void main(String[] args)
+    {
+        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("AddressBook App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("AddressBook App DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
+
 }
